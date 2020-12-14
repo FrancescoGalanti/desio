fe35998839534652cc7eb99a5c3c1dc9607b7590
FrancescoGalanti <galanti_francesco>
2020-12-14 15:34:51 +0100

    stampata la lunghezza della stringa

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Badwords</title>
 </head>
 <body>
 
@@ -12,7 +12,18 @@
     <?php
      /* variable text */
      $txt = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos libero enim debitis rem distinctio. Veniam libero nam aperiam sit possimus, et eligendi eius placeat amet, soluta labore beatae, architecto accusantium!";
-     
+    
      ?>
+      <!-- printing variable in html -->
+     <p>Benvenuto <?=$txt?></p>
+
+     <?php
+      /* Printing the text.lenght */
+      echo strlen($txt);
+      ?>
+
+     
+
+
 </body>
 </html>
